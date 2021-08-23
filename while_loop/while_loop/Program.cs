d9f52f05a9a3996e14d0408fee54c398284ccec1
saptarshibanerjee8335 <1sappy8335@gmail.com>
2021-08-23 15:38:48 +0530

    loop1

---
+++ while_loop/while_loop/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace while_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            System.Console.WriteLine("Number from 1 to 10");
+            while(a<=10)
+            {
+                System.Console.WriteLine("print " + a);
+                a++;
+            }
+        }
+    }
+}
