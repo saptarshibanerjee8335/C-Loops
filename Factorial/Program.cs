47ab4b6d7a1c01d7c9fdf2b2513b535ba51f592e
saptarshibanerjee8335 <1sappy8335@gmail.com>
2021-08-23 16:05:05 +0530

    loops

---
+++ Factorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, fact = 1, number;
+            Console.Write("Enter any number");
+            number = int.Parse(Console.ReadLine());
+            for(i=1;i<=number;i++)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine(fact);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ loops/loops/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 1, c, i, num;
+            Console.WriteLine("Enter the number of elements");
+            num = int.Parse(Console.ReadLine());
+            Console.Write(a + " " + b + " ");
+            for (i = 2; i < num; i++)
+            {
+                c = a + b;
+                Console.Write(c+" ");
+                a = b;
+                b = c;
+            }
+        }
+
+    }
+}
